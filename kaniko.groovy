9f0fe8b8ccf66f9debe21774d6775b6e98b46799
andrz <andrzej_00@interia.eu>
2024-02-03 14:28:36 +0100

    init

--- kaniko.groovy
+++ kaniko.groovy
@@ -1,4 +1,5 @@
 def DOCKER_REGISTRY = '74734589924'
+
 pipeline {
     agent {
         kubernetes {
@@ -7,31 +8,48 @@ pipeline {
         }
     }
 
-
-    stage('Get a Maven project') {
-        git url: 'https://github.com/scriptcamp/kubernetes-kaniko.git', branch: 'main'
-        container('maven') {
-            stage('Build a Maven project') {
-                sh '''
-                        echo pwd
-                        '''
-            }
-        }
-    }
-
-    stage('Build Java Image') {
-        container('kaniko') {
-            stage('Build a Go project') {
-                sh """
-            /kaniko/executor --context `pwd` \
-                             --dockerfile=Dockerfile \
-                             --cache=false \
-                             --verbosity=info \
-                             --log-format=color \
-                             --log-timestamp=true \
-                             --destination=${DOCKER_REGISTRY}/hello-kaniko:1.0
-                        """
+    stages {
+        stage('Run maven') {
+            steps {
+                sh 'mvn -version'
             }
         }
     }
 }
+//
+//pipeline {
+//    agent {
+//        kubernetes {
+//            defaultContainer 'maven'
+//            yamlFile 'kaniko.yaml'
+//        }
+//    }
+//
+//
+//    stage('Get a Maven project') {
+//        git url: 'https://github.com/scriptcamp/kubernetes-kaniko.git', branch: 'main'
+//        container('maven') {
+//            stage('Build a Maven project') {
+//                sh '''
+//                        echo pwd
+//                        '''
+//            }
+//        }
+//    }
+//
+//    stage('Build Java Image') {
+//        container('kaniko') {
+//            stage('Build a Go project') {
+//                sh """
+//            /kaniko/executor --context `pwd` \
+//                             --dockerfile=Dockerfile \
+//                             --cache=false \
+//                             --verbosity=info \
+//                             --log-format=color \
+//                             --log-timestamp=true \
+//                             --destination=${DOCKER_REGISTRY}/hello-kaniko:1.0
+//                        """
+//            }
+//        }
+//    }
+//}
