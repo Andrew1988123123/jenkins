9f40baf3c37cb0c42680fa943323b42ab24a0b88
andrz <andrzej_00@interia.eu>
2024-02-03 14:09:37 +0100

    init

---
+++ kaniko.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent {
+        kubernetes {
+            defaultContainer 'maven'
+            yamlFile 'kaniko.yaml'
+        }
+
+        def DOCKER_REGISTRY = '74734589924'
+
+        stages {
+            stage('Get a Maven project') {
+                git url: 'https://github.com/scriptcamp/kubernetes-kaniko.git', branch: 'main'
+                container('maven') {
+                    stage('Build a Maven project') {
+                        sh '''
+                        echo pwd
+                        '''
+                    }
+                }
+            }
+
+            stage('Build Java Image') {
+                container('kaniko') {
+                    stage('Build a Go project') {
+                        sh """
+            /kaniko/executor --context `pwd` \
+                             --dockerfile=Dockerfile \
+                             --cache=false \
+                             --verbosity=info \
+                             --log-format=color \
+                             --log-timestamp=true \
+                             --destination=${DOCKER_REGISTRY}/hello-kaniko:1.0
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
