d1fb2512c05b54fbf74d9867320f9666edd88a12
andrz <andrzej_00@interia.eu>
2024-02-03 14:26:41 +0100

    init

--- kaniko.groovy
+++ kaniko.groovy
@@ -7,53 +7,31 @@ pipeline {
         }
     }
 
-    stages {
-        stage('Get a Maven project') {
-            git url: 'https://github.com/scriptcamp/kubernetes-kaniko.git', branch: 'main'
-            container('maven') {
-                stage('Build a Maven project') {
-                    sh '''
+
+    stage('Get a Maven project') {
+        git url: 'https://github.com/scriptcamp/kubernetes-kaniko.git', branch: 'main'
+        container('maven') {
+            stage('Build a Maven project') {
+                sh '''
                         echo pwd
                         '''
-                }
+            }
+        }
+    }
+
+    stage('Build Java Image') {
+        container('kaniko') {
+            stage('Build a Go project') {
+                sh """
+            /kaniko/executor --context `pwd` \
+                             --dockerfile=Dockerfile \
+                             --cache=false \
+                             --verbosity=info \
+                             --log-format=color \
+                             --log-timestamp=true \
+                             --destination=${DOCKER_REGISTRY}/hello-kaniko:1.0
+                        """
             }
         }
     }
 }
-
-//pipeline {
-//    agent {
-//        kubernetes {
-//            defaultContainer 'maven'
-//            yamlFile 'kaniko.yaml'
-//        }
-//    }
-//    stages {
-//        stage('Get a Maven project') {
-//            git url: 'https://github.com/scriptcamp/kubernetes-kaniko.git', branch: 'main'
-//            container('maven') {
-//                stage('Build a Maven project') {
-//                    sh '''
-//                        echo pwd
-//                        '''
-//                }
-//            }
-//        }
-//
-//        stage('Build Java Image') {
-//            container('kaniko') {
-//                stage('Build a Go project') {
-//                    sh """
-//            /kaniko/executor --context `pwd` \
-//                             --dockerfile=Dockerfile \
-//                             --cache=false \
-//                             --verbosity=info \
-//                             --log-format=color \
-//                             --log-timestamp=true \
-//                             --destination=${DOCKER_REGISTRY}/hello-kaniko:1.0
-//                        """
-//                }
-//            }
-//        }
-//    }
-//}
