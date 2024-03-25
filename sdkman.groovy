90f395a7d5c4872adf4b5ecec4ba3613c047ab03
andrz <andrzej_00@interia.eu>
2024-03-25 17:14:49 +0100

    add sdkman

---
+++ sdkman.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent {
+        kubernetes {
+            defaultContainer 'sdkman'
+            yamlFile 'sdkman.yaml'
+        }
+    }
+
+    stages {
+        stage('Run git') {
+            steps {
+                git url: 'https://github.com/Andrew1988123123/jenkins.git', branch: 'master'
+                container('sdkman') {
+                    sh 'echo `pwd`'
+                    sh 'whoami'
+                }
+            }
+        }
+
+        stage('Run sdkman') {
+            steps {
+                sh 'sdk version'
+            }
+        }
+
+        stage('Build version') {
+            steps {
+                sh """
+                      mvn -v
+                       """
+            }
+        }
+    }
+}
